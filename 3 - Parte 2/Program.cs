36c9813c4973237e0d33fb0160028a50d80a7b00
Matheus <matheusoliveiradesousapereira@gmail.com>
2025-12-04 02:07:14 -0300

    Add projects 3 and 3 - Parte 2 with input processing
    Added a new project `3 - Parte 2` to calculate a "Part 2 Password" by processing an `input.txt` file, extracting specific numbers, and summing them. Configured the project to use .NET 10.0, with nullable reference types and implicit usings enabled. The `input.txt` file is set to always copy to the output directory.
    
    Added another project `3` to calculate a "Part 2 Password" by finding the two largest numbers in each line of `input.txt`, combining them, and summing the results. Configured similarly to `3 - Parte 2`.
    
    Updated the `AdventOOfCode.slnx` solution file to include both new projects. Added a large `input.txt` file containing numeric data for use in both projects.

---
+++ 3 - Parte 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+const string fileName = "input.txt";
+
+if (!File.Exists(fileName))
+    return;
+
+long sum = 0; 
+
+foreach (string line in File.ReadLines(fileName))
+{
+    char[] numbers = line.ToArray();
+
+    string currentNumberResult = "";
+    int lastIndex = -1; 
+
+    for (int i = 0; i < 12; i++)
+    {
+        int digitsNeededAfter = 11 - i;
+
+        var nextMax = numbers
+            .Take(numbers.Length - digitsNeededAfter)
+            .Select((val, index) => new { Value = val, Index = index })
+            .Where(x => x.Index > lastIndex)
+            .OrderByDescending(x => x.Value)
+            .First();
+
+        currentNumberResult += nextMax.Value;
+        lastIndex = nextMax.Index;
+    }
+
+    sum += long.Parse(currentNumberResult);
+}
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine($"Part 2 Password (Total sum): {sum}");
+Console.WriteLine("--------------------------------------------------");
---
+++ 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+const string fileName = "input.txt";
+
+if (!File.Exists(fileName))
+    return;
+
+string[] instructions = File.ReadAllLines(fileName);
+
+int sum = 0;
+foreach (string line in File.ReadLines(fileName))
+{
+    char[] numbers = line.ToArray();
+
+    var numberOneMax = numbers
+        .Take(numbers.Length - 1)
+        .Select((val, index) => new { Value = val, Index = index })
+        .OrderByDescending(x => x.Value)
+        .First();
+    var indexNumberOneMax = numberOneMax.Index;
+    var maxValueNumberOne = numberOneMax.Value;
+
+    char numberTwoMaxFound = numbers.Skip(indexNumberOneMax + 1).Max();
+    int numberCombinationMax = int.Parse(maxValueNumberOne.ToString() + numberTwoMaxFound.ToString());
+
+    sum += numberCombinationMax;
+}
+
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine($"Part 2 Password (Total zeros): {sum}");
+Console.WriteLine("--------------------------------------------------");
