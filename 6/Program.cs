23bfbdab229e607bbd7ad7ac39d524fe84d23337
Matheus <matheusoliveiradesousapereira@gmail.com>
2025-12-07 03:54:48 -0300

    Add project 6: column-wise sum/product calculator
    Added new "6" project to solution targeting .NET 10.0. Implemented Program.cs to read input.txt, apply column-wise "+" or "*" operations, and output the total sum. Included input.txt with data and operation row, and configured resource copying in 6.csproj.

---
+++ 6/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+const string fileName = "input.txt";
+
+if (!File.Exists(fileName))
+    return;
+var lines = File.ReadAllLines(fileName)
+    .Where(line => !string.IsNullOrWhiteSpace(line))
+    .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+    .ToArray();
+
+long sum = 0;
+int totalColunas = lines[0].Length;
+int totalLinhas = lines.Length;
+for (int i = 0; i < totalColunas; i++)
+{
+    var operacao = "";
+    var numbers = new List<long>();
+    for (int j = 1; j <= lines.Length; j++)
+    {
+        if (j == lines.Length)
+        { 
+            operacao = lines[j - 1][i];
+        }
+        else
+            numbers.Add(long.Parse(lines[j - 1][i]));
+    }
+    sum+= ExecuteOperation(numbers, operacao);
+}
+
+Console.WriteLine(sum);
+long ExecuteOperation(List<long> numbers, string operation)
+{
+    return operation switch
+    {
+        "+" => numbers.Sum(),
+        "*" => numbers.Aggregate((acc, x) => acc * x),
+        _ => throw new InvalidOperationException("Operação inválida")
+    };
+} 
